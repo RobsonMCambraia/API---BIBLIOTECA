14877d9933bc4fd84c18cfbcc5df03bfeff16886
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-02 23:32:26 -0300

    metodos GET, POST E PUT

--- main.py
+++ main.py
@@ -8,13 +8,27 @@ df_func = pd.read_excel("scr/BaseFuncionarios.xlsx")
 dict_func = df_func.to_dict(orient='records')
 
 @app.route('/funcionarios', methods=['GET'])
-def get_funcionarios():
+def getFuncionarios():
     return jsonify(dict_func)
 
-@app.route('funcionarios/<int:id>', methods=['POST'])
-def get_func_por_id(id):
+@app.route('/funcionarios/<int:id>', methods=['GET'])
+def getFunc_por_id(id):
     for funcionario in dict_func: 
         if funcionario.get('ID RH') == id:
             return jsonify(funcionario)
+
+@app.route('/funcionarios/<int:id>', methods=['PUT'])
+def putAdd_func(id):
+    func_alt = request.get_json()
+    for indice, funcionario in enumerate(dict_func):
+        if funcionario.get('ID RH') == id:
+            dict_func[indice].update(func_alt)
+            return jsonify(dict_func[indice])
+
+@app.route('/funcionarios', methods=['POST'])
+def postFunc():
+    novo_func = request.get_json()
+    dict_func.append(novo_func)
+    return jsonify(dict_func) 
 
 app.run(port=5000, host='localhost', debug=True)
