059012550d4952f3a8b2b9b54b70cf39f9ee5633
RobsonMCambraia <robson.mcambraia23@gmail.com>
2024-03-02 21:09:31 -0300

    BUSCA POR ID

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import pandas as pd
 # cria uma instância da classe Flask e a associa ao nome do seu script ou módulo
 app = Flask(__name__)
 
-df_funcionarios = pd.read_excel("scr/BaseFuncionarios.xlsx")
-dict_funcionarios = df_funcionarios.to_dict(orient='records')
+df_func = pd.read_excel("scr/BaseFuncionarios.xlsx")
+dict_func = df_func.to_dict(orient='records')
 
 @app.route('/funcionarios', methods=['GET'])
 def get_funcionarios():
-    return jsonify(dict_funcionarios)
+    return jsonify(dict_func)
 
+@app.route('funcionarios/<int:id>', methods=['POST'])
 def get_func_por_id(id):
-    pass
+    for funcionario in dict_func: 
+        if funcionario.get('ID RH') == id:
+            return jsonify(funcionario)
 
 app.run(port=5000, host='localhost', debug=True)
